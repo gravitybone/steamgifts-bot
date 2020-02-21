e689c39b1dc71594ef74cdd43c3d92eeedba0232
Marcel Budoj <marcel.budoj@googlemail.com>
2020-02-21 18:24:07 +0100

    rework program structure

--- main.py
+++ main.py
@@ -6,40 +6,77 @@ from requests import RequestException
 from random import randint
 from time import sleep
 
-try:
-    file = open('cookie.txt', 'r')
-    cook = file.readline()
-    if len(cook) == 0:
-        print('There is no cookie in cookie.txt file')
-        sleep(30)
-        sys.exit(0)
-except FileNotFoundError:
-    print('Cant find cookie.txt file')
-    sleep(30)
-    sys.exit(0)
-
-pages = 1
-cookies = {}
 xsrf_token = 0
 points = 0
 
 
-def get_soup_from_page(url):
-    global cookies
+def get_cookie():
+    try:
+        file = open('cookie.txt', 'r')
+        cook = file.readline()
+        if len(cook) == 0:
+            print('There is no cookie in cookie.txt file')
+            sys.exit(0)
+        cookies = {'PHPSESSID': cook}
+        return cookies
+    except FileNotFoundError:
+        print('Cant find cookie.txt file')
+        sys.exit(0)
 
-    cookies = {'PHPSESSID': cook}
-    r = requests.get(url, cookies=cookies)
+
+def get_soup_from_page(url, cookie):
+    r = requests.get(url, cookies=cookie)
     return BeautifulSoup(r.text, 'html.parser')
 
 
-def get_page():
-    global xsrf_token, points
+# enter all games on one site
+def enter_games(page, cookies):
+    soup = get_soup_from_page(page, cookies)
 
     try:
-        soup = get_soup_from_page('https://www.steamgifts.com')
+        gifts_list = soup.find_all(lambda tag: tag.name == 'div' and tag.get('class') == ['giveaway__row-inner-wrap'])
 
+        for item in gifts_list:
+            game_cost = item.find_all('span', {'class': 'giveaway__heading__thin'})
+
+            last_div = None
+            for last_div in game_cost:
+                pass
+            if last_div:
+                game_cost = last_div.getText().replace('(', '').replace(')', '').replace('P', '')
+
+            game_name = item.find('a', {'class': 'giveaway__heading__name'}).text.encode('utf-8')
+
+            if points - int(game_cost) < 0:
+                print('Not enough points to enter: ' + game_name.decode("utf-8"))
+                continue
+            elif points - int(game_cost) > 0:
+                enter_giveaway(item.find('a', {'class': 'giveaway__heading__name'})['href'].split('/')[2], game_name,
+                               cookies)
+    except AttributeError as e:
+        return
+
+
+# get codes of the games
+def get_games(cookie):
+    print("Proccessing games from wishlist.")
+    enter_games("https://www.steamgifts.com/giveaways/search?type=wishlist", cookie)
+    if points > 5:
+        print("Proccessing all games.")
+        enter_games("https://www.steamgifts.com", cookie)
+        n = 2
+        while points > 5:
+            enter_games("https://www.steamgifts.com/giveaways/search?page=" + str(n), cookie)
+            n += 1
+    print('Finished')
+
+
+def get_page(cookie):
+    global xsrf_token, points
+    try:
+        soup = get_soup_from_page('https://www.steamgifts.com', cookie)
         xsrf_token = soup.find('input', {'name': 'xsrf_token'})['value']
-        points = soup.find('span', {'class': 'nav__points'}).text  # storage points
+        points = int(soup.find('span', {'class': 'nav__points'}).text)  # storage points
     except RequestException:
         print('Cant connect to the site')
         print('Waiting 2 minutes and reconnect...')
@@ -47,60 +84,13 @@ def get_page():
         get_page()
     except TypeError:
         print('Cant recognize your cookie value.')
-        sleep(30)
         sys.exit(0)
 
 
-# get codes of the games
-def get_games():
-    global pages
-
-    n = 1
-    while n <= pages:
-        print('Proccessing games from %d page.' % n)
-
-        soup = get_soup_from_page('https://www.steamgifts.com/giveaways/search?page=' + str(n) + "&type=wishlist")
-
-        try:
-            gifts_list = soup.find_all(
-                lambda tag: tag.name == 'div' and tag.get('class') == ['giveaway__row-inner-wrap'])
-
-            for item in gifts_list:
-                if int(points) == 0:
-                    print('> Sleeping to get 6 points')
-                    sleep(900)
-                    get_games()
-                    break
-
-                game_cost = item.find_all('span', {'class': 'giveaway__heading__thin'})
-
-                last_div = None
-                for last_div in game_cost:
-                    pass
-                if last_div:
-                    game_cost = last_div.getText().replace('(', '').replace(')', '').replace('P', '')
-
-                game_name = item.find('a', {'class': 'giveaway__heading__name'}).text.encode('utf-8')
-
-                if int(points) - int(game_cost) < 0:
-                    print('Not enough points to enter: ' + game_name.decode("utf-8"))
-                    continue
-                elif int(points) - int(game_cost) > 0:
-                    entry_gift(item.find('a', {'class': 'giveaway__heading__name'})['href'].split('/')[2], game_name)
-
-            n = n + 1
-        except AttributeError as e:
-            break
-
-    print('List of games ended. Exiting')
-
-
-def entry_gift(code, game_name):
+def enter_giveaway(code, game_name, cookies):
     payload = {'xsrf_token': xsrf_token, 'do': 'entry_insert', 'code': code}
     entry = requests.post('https://www.steamgifts.com/ajax.php', data=payload, cookies=cookies)
     json_data = json.loads(entry.text)
-
-    get_page()
 
     # updating points after entered a giveaway
     if json_data['type'] == 'success':
@@ -110,6 +100,11 @@ def entry_gift(code, game_name):
         sleep(sleep_time)
 
 
+def main():
+    cookie = get_cookie()
+    get_page(cookie)
+    get_games(cookie)
+
+
 if __name__ == '__main__':
-    get_page()
-    get_games()
+    main()
